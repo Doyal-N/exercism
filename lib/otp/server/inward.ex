2de79f55294b51e7008e482970319707ff9e98f1
Doyal-N <coin20@protonmail.com>
2021-11-07 20:11:07 +0500

    Fix code

--- lib/otp/server/inward.ex
+++ lib/otp/server/inward.ex
@@ -8,29 +8,29 @@ defmodule Server.Inward do
 
   def handle_call({:add, num}, _from, state) do
     case Logic.add(state, num) do
-      {:ok, result} -> {:reply, result, result}
-      {:error, result} -> {:reply, result, state}
+      {:ok, result} -> {:reply, {:ok, result}, result}
+      {:error, result} -> {:reply, {:error, result}, state}
     end
   end
 
   def handle_call({:mult, num}, _from, state) do
     case Logic.mult(state, num) do
-      {:ok, result} -> {:reply, result, result}
-      {:error, result} -> {:reply, result, state}
+      {:ok, result} -> {:reply, {:ok, result}, result}
+      {:error, result} -> {:reply, {:error, result}, state}
     end
   end
 
   def handle_call({:div, num}, _from, state) do
     case Logic.divide(state, num) do
-      {:ok, result} -> {:reply, result, result}
-      {:error, :division_by_zero} -> {:reply, IO.puts("Error. Division by zero"), state}
-      {:error, result} -> {:reply, result, state}
+      {:ok, result} -> {:reply, {:ok, result}, result}
+      {:error, :division_by_zero} -> {:reply, {:error, "division by zero"}, state}
+      {:error, result} -> {:reply, {:error, result}, state}
     end
   end
 
   def handle_call(:clean, _from, _state) do
     result = Logic.clean()
-    {:reply, result, result}
+    {:reply, {:ok, result}, result}
   end
 
   def terminate(_reason, state) do
